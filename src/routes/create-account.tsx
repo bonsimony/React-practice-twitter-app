22429b9c1a0e76db9f073aad3947bd5906196252
bonsimony <hbon7270@gmail.com>
2025-04-14 17:43:30 +0900

    Create Account

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,3 +1,123 @@
-export default function CreateAccount(){
-    return <h1>create accrount</h1>;
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useState } from "react";
+import { styled } from "styled-components";
+import { auth } from "./firebase";
+import { useNavigate } from "react-router-dom";
+
+const Wrapper = styled.div`
+  height: 100vh;                          /* 화면 전체 높이를 기준으로 중앙 정렬 */ 
+  display: flex;
+  justify-content: center;                /* 수평 중앙 */
+  align-items: center;                    /* 수직 중앙 */
+  flex-direction: column;
+  width: 420px;
+  margin: 0 auto;                         /* 화면 가운데로 */
+  padding: 50px 0px;
+
+`;
+
+const Title = styled.h1`
+  font-size: 42px;
+`;
+
+const Form = styled.form`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+  &[type="submit"] {
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
+const Error = styled.span`
+  font-weight: 600;
+  color: tomato;
+`;
+
+export default function CreateAccount() {
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if(isLoading === true ||name === "" || email === "" || password === ""){
+      return;
+    }
+    try {
+      setLoading(true);
+      const credentials = await createUserWithEmailAndPassword(auth, email, password)
+                                                              // createUserWithEmailAndPassword는 Firebase Auth SDK에서 제공하는 함수로 인자에 auth 인스턴스가 필요하다.
+                                                              // firebase.ts 파일 내 auth 인스턴스가 생성되어 있음!
+      await updateProfile(credentials.user, {displayName : name});  
+      navigate("/");                                                            
+    } catch (e) {
+     
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <Wrapper>
+      <Title>Join 𝕏</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name="name"
+          value={name}
+          placeholder="Name"
+          type="text"
+          required
+        />
+        <Input
+          onChange={onChange}
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          required
+        />
+        <Input
+          onChange={onChange}
+          value={password}
+          name="password"
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <Input
+          type="submit"
+          value={isLoading ? "Loading..." : "Create Account"}
+        />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
+    </Wrapper>
+  );
 }
